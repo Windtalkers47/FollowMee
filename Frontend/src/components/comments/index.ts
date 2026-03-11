fd0f638b9b2163492ea6fc95d425894dd618c3f6
Windtalkers47 <chodsukkasem.pi@mandala.co.th>
2026-03-11 13:28:31 +0700

    Try to Rework Comment System Architecture

--- Frontend/src/components/comments/index.ts
+++ Frontend/src/components/comments/index.ts
@@ -1,9 +1,4 @@
-// Main comment components
+// YouTube-style comment system
 export { default as CommentTree } from './CommentTree';
-export { default as ThreadedCommentNode } from './ThreadedCommentNode';
-export { ThreadColumn } from './ThreadColumn';
-
-// Virtualized components
-export { default as VirtualizedCommentTree } from './VirtualizedCommentTree';
-export { default as DynamicVirtualizedCommentList } from './DynamicVirtualizedCommentList';
-export { VirtualizedCommentList } from './VirtualizedCommentList';
+export { default as YouTubeCommentNode } from './YouTubeCommentNode';
+export { default as YouTubeThreadedRow } from './YouTubeThreadedRow';
